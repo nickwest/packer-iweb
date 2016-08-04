e5b990582b29ab540796587b96dd49b9e7b59a2e
Nick West <nickwest@uw.edu>
2016-08-04 13:57:24 -0700

    fixed a path typo in stunnel.sh

--- scripts/stunnel.sh
+++ scripts/stunnel.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Copy the stunnel service file
-cp /home/vagrant/stunnel.service etc/systemd/system/stunnel.service
+cp /home/vagrant/stunnel.service /etc/systemd/system/stunnel.service
 rm /home/vagrant/stunnel.service
 
 # Add an stunnel user
