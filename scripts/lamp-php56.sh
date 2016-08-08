46ae8c004819147d62ba4ff4712a712423f46310
Nick West <nickwest@uw.edu>
2016-08-08 08:37:49 -0700

    Added a PHP 5.6 version + cleaned up file names + hostname change

---
+++ scripts/lamp-php56.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+## Install Additional repos & LAMP
+
+yum -y install epel-release
+yum -y install https://centos7.iuscommunity.org/ius-release.rpm # Apache, MariaDB, PHP 7 repo
+
+yum -y swap -- install mariadb101u-libs -- remove mariadb-libs
+
+yum -y install mariadb101u-server
+yum -y install httpd24u httpd24u-mod_ssl gd
+yum -y install php56u-json php56u-cli php56u-gd php56u php56u-mcrypt php56u-mbstring php56u-bcmath php56u-pdo php56u-pdo_mysql php56u-dom
+
+systemctl enable httpd
+systemctl enable mariadb
+
+## Convenience & Admin
+yum -y install htop atop screen tmux vim stunnel nano git sendmail wget
