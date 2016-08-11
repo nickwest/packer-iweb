052de669164fe332a86ccb6dcf0c7fd4a0c167e8
Nick West <nickkwest@gmail.com>
2016-08-11 14:19:58 -0700

    Fix for vagrant user's composer vendor path in PATH var

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -11,7 +11,7 @@ then
     RESULT=$?
     rm composer-setup.php
 	mv composer.phar /usr/local/bin/composer
-    sed -i 's/export PATH/PATH=$PATH:\/usr\/local\/sbin:$HOME/\.config\/composer\/vendor\/bin\nexport PATH/' /home/vagrant/.bash_profile
+    sed -i 's/export PATH/PATH=$PATH:\/usr\/local\/sbin:\$HOME\/\.config\/composer\/vendor\/bin\nexport PATH/' /home/vagrant/.bash_profile
     exit $RESULT
 else
     >&2 echo 'ERROR: Invalid installer signature'
