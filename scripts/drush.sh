af353235953c10e6cbd054314b5a5fc1eab9223c
Nick West <nickwest@uw.edu>
2016-09-22 10:27:09 -0700

    Added drupal.phar installation

--- scripts/drush.sh
+++ scripts/drush.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 
 su vagrant -c "/usr/local/bin/composer global require drush/drush:8.*"
+
+sudo curl https://drupalconsole.com/installer -L -o /usr/local/bin/drupal
+sudo chmod 775 /usr/local/bin/drupal
